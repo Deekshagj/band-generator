a0d0ad71a938fb3dba6abbd0818dc83cbcf13dd6
Deeksha J <126661739+Deekshagj@users.noreply.github.com>
2023-11-27 21:04:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print("Welcome to the band name generator!\n")
 color=input("Which is your favourite colour?\n")
 pet=input("What's your pet's name?\n")
-print("Here's what could be your new band name!!!"+color+" "+pet)
+print("Here's what could be your new band name:"+color+" "+pet)
